bf828f9a4202d8973e1f6765736984dad5604c28
Starovoytov Alexandr <saniy200311@gmail.com>
2021-08-05 12:58:53 +0300

    script to count Gosha's place

--- bmstu-iu9.sh
+++ bmstu-iu9.sh
@@ -9,4 +9,9 @@ wget -q \
 pdftotext -layout -nopgbrk ./01.03.02.pdf
 rm -f ./01.03.02.pdf || true
 
-places=$(sed 's/^.*\([0-9]\+\) мест.*$/\1/' < 01.03.02.txt)
+places=$(grep "мест, ост" 01.03.02.txt | sed 's/.* \([0-9]*\) мест.*/\1/')
+
+current=$(tail + $(sed -n '/14 мест/=' 01.03.02.txt) 01.03.02.txt | sed '/183-084-710 72/Q' | awk '{print $12}' | grep -c "Да")
+((current++))
+
+echo "$current/$places"
