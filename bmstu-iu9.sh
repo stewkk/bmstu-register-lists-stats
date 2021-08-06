20424e7661f55bafff82216f25356667ba997e02
Starovoytov Alexandr <saniy200311@gmail.com>
2021-08-06 20:02:36 +0300

    bugfix

--- bmstu-iu9.sh
+++ bmstu-iu9.sh
@@ -9,9 +9,12 @@ wget --output-document=./01.03.02.pdf --append-output=./wget.log \
 
 pdftotext -layout -nopgbrk ./01.03.02.pdf # TODO сделать лог pdftotext
 
-places=$(grep "мест, ост" 01.03.02.txt | sed 's/.* \([0-9]*\) мест.*/\1/')
+places=$(grep "1. Поступающие на места в рамках КЦП по общему конкурсу" \
+    01.03.02.txt | sed 's/.* \([0-9]*\) мест.*/\1/')
 
-current=$(tail +$(sed -n '/мест, ост/=' 01.03.02.txt) 01.03.02.txt | \
+current=$(tail \
+    +$(sed -n '/1. Поступающие на места в рамках КЦП по общему конкурсу/=' \
+    01.03.02.txt) 01.03.02.txt | \
     sed '/183-084-710 72/Q' | awk '{print $12}' | grep -c "Да") || true
 current=$((current + 1))
 
